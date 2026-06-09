d9f99723bfe434800475f57c1e7194e4e1849d14
Washington-ux-ux <lyniker21@hotmail.com>
2026-06-09 11:21:42 -0300

    teste 2

--- src/controllers/postsControllers.ts
+++ src/controllers/postsControllers.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express"
 import * as service from "../services/postsServices"
 import * as fs from 'fs'
 import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const getPosts = async (req: Request, res: Response) => {
     const response = await service.getPostsService()
