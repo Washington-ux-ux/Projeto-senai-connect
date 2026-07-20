f78d2f8c063443673e4c819ed170e46f0c2d525a
Washington-ux-ux <lyniker21@hotmail.com>
2026-07-20 20:42:50 -0300

    Corrigir problema de imagens - usar caminho ./assets/uploads/ e adicionar rota estática /assets

--- app.ts
+++ app.ts
@@ -15,6 +15,7 @@ function createApp() {
     app.use(express.urlencoded({ extended: true, limit: '50mb' }));
     
     app.use(express.static(path.join(__dirname, 'docs')));
+    app.use('/assets', express.static(path.join(__dirname, 'docs/assets')));
     app.use('/api', router);
 
     console.log("App created successfully");
